31f15bb6cb2f11c3fd19d98382398ea700c22d76
Ism_g <ismail.guedira@gmail.com>
2015-10-14 19:20:04 +0200

    Nouveau dossier = > Compiler du Latex Proprement ! 	- Compile le latex simplement en effacant les fichiers chiant sauf le .toc qui est tres important ! 	- Compile en donnant au pdf un autre nom si tu veux  ! ATTENTION : les fichiers supprimer peuvent etre important DEBUG : POurquoi pas les mettre dans un dossier ou il sait qu'il sont la ____> A CHERCHER !

---
+++ compiler_latex_progra/MakeLatex.sh
@@ -0,0 +1,17 @@
+OUI='y'
+NON='n'
+
+echo "\n Salut ! Compilons ensemble du LATEX \n"
+echo "\n Quel est le nom du fichier ??? (n'oublie pas de mettre le .tex ) \n "
+read -r $choix1
+echo "\n Je le compile avec le meme nom ?"
+read -r choix2
+if [[ "$choix2" == $OUI ]]; then
+  pdflatex $choix1
+  rm *.aux *.out *.log *.fls *.lot *.synctex.gz *.lof *.fdb_latexmk
+else echo "\n Quel est le nouveau nom ?\n"
+  read -r $choix3
+  pdflatex -jobname=$choix3 $choix1
+  rm *.aux *.out *.log *.fls *.lot *.synctex.gz *.lof *.fdb_latexmk
+fi
+
