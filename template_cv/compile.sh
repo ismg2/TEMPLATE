f03295b3e6c3b848ecfdde84dce8011cc62fccce
Ism_g <ismail.guedira@gmail.com>
2015-10-29 15:07:38 +0100

    on change le script en consequence

--- template_cv/compile.sh
+++ template_cv/compile.sh
@@ -18,11 +18,10 @@ cd template_cv/
 
 ################################### On copie les CV en anglais et en francais on c jamais et on corrige/compile ####
 
-cp cv1.tex $b;
-cp cv1_english.tex $b;
+cp TEX/cv1.tex $b;
+cp TEX/cv1_english.tex $b;
 cp photo.png $b;
 printf "\n CV (fr et english) en latex copier dans le dossier %s \n" "$b"
-cp LM.tex $b; 
 printf "\n LM (en anglais) générique en latex copier dans le dossier %s \n" "$b"
 printf "\n Tu veux modifier le CV pour rajouter le sujet du stage ? ( y or n) \n"
 read -r choix1
@@ -41,7 +40,7 @@ if [ "$choix1" == $NON ];then
     open CV_Ismail_Guedira_en.pdf;
   fi
 else echo "\n la reponse est oui du coup je t'ouvre le fichier \n"
-  open cv1.tex
+  atom TEX/cv1.tex
   echo "/n bon j'attend apres je compile hein !!!"
   read -r compilation_cv
   if [ "$compilation_cv" == $OUI ]; then
@@ -56,21 +55,21 @@ cd ..;
 printf "\n\n\n Maintenant passons a la lettre de motivation \n\n\n"
 printf "\n  Candidature en francais  \n"
 read -r choix3
-if [ "$choix3" == $OUI];then
-  cp LM_fr.tex $b;
+if [ "$choix3" == $OUI];then # Version Francaise
+  cp TEX/LM_fr.tex $b;
   atom $b/LM_fr.tex;
   printf "\nje te laisse la corriger et la compiler hein\n"
   printf "\n appuye sur (y) quand tu as finis que je la compiles dans le bon titre !! :D !!! \n"
   read -r compilation_f
-  if [ "$compilation_f" == $OUI ]; then
+  if [ "$compilation_f" == $OUI ]; then # Version Francaise
     pdflatex -jobname=LM_Ismail_Guedira LM_fr.tex
   fi
-else
-  cp LM_en.tex $b;
+else # Version Anglais
+  cp TEX/LM_en.tex $b; 
   atom $b/LM_en.tex; 
   printf "\nje te laisse la corriger et j'attend\n"
   printf "\nappuye sur (y) quand tu as finis que je la compiles dans le bon titre !! :D !!!\n"
-  read -r compilation_f
+  read -r compilation_f # Version Anglais
   if [ "$compilation_f" == $OUI ]; then
     pdflatex -jobname=CL_Ismail_Guedira LM_en.tex
   fi
